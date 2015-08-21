3d27a6fd81f3c228b9326a489e9ca24e88bd49a2
Avinash <avinashghare572@gmail.com>
2015-08-21 13:36:58 +0530

    First git for access backend

---
+++ application/models/brand_model.php
@@ -0,0 +1,72 @@
+<?php
+if ( !defined( 'BASEPATH' ) )
+	exit( 'No direct script access allowed' );
+class brand_model extends CI_Model
+{
+	//brand
+	public function createbrand($name,$order)
+	{
+		$data  = array(
+			'name' => $name,
+			'order' => $order,
+		);
+		$query=$this->db->insert( 'brand', $data );
+		return  1;
+	}
+	function viewbrand()
+	{
+		$query=$this->db->query("SELECT `brand`.`id`,`brand`.`name`,`brand`.`order` FROM `brand` 
+		ORDER BY `brand`.`id` ASC")->result();
+		return $query;
+	}
+	public function beforeeditbrand( $id )
+	{
+		$this->db->where( 'id', $id );
+		$query=$this->db->get( 'brand' )->row();
+		return $query;
+	}
+	
+	public function editbrand( $id,$name,$order)
+	{
+		$data = array(
+			'name' => $name,
+			'order' => $order,
+		);
+		$this->db->where( 'id', $id );
+		$this->db->update( 'brand', $data );
+		return 1;
+	}
+	function deletebrand($id)
+	{
+		$query=$this->db->query("DELETE FROM `brand` WHERE `id`='$id'");
+	}
+	public function getbranddropdown()
+	{
+		$query=$this->db->query("SELECT * FROM `brand`  ORDER BY `id` ASC")->result();
+		$return=array(
+		);
+		foreach($query as $row)
+		{
+			$return[$row->id]=$row->name;
+		}
+		
+		return $return;
+	}
+    public function getbrandbyproduct($id)
+	{
+         $return=array();
+		$query=$this->db->query("SELECT `id`,`product`,`brand` FROM `productbrand`  WHERE `product`='$id'");
+        if($query->num_rows() > 0)
+        {
+            $query=$query->result();
+            foreach($query as $row)
+            {
+                $return[]=$row->brand;
+            }
+        }
+         return $return;
+         
+		
+	}
+}
+?>
